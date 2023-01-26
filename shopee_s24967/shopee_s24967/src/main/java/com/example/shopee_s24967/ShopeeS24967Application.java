2ceb49506e2c40fe3b6e62b70c8f8bf9ee55bf51
Daria Szablowska <s24967@pjwstk.edu.pl>
2023-01-26 18:08:05 +0100

    first commit1

---
+++ shopee_s24967/shopee_s24967/src/main/java/com/example/shopee_s24967/ShopeeS24967Application.java
@@ -0,0 +1,13 @@
+package com.example.shopee_s24967;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeeS24967Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeeS24967Application.class, args);
+	}
+
+}
---
+++ shopee_s24967/shopee_s24967/src/test/java/com/example/shopee_s24967/ShopeeS24967ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopee_s24967;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeeS24967ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
